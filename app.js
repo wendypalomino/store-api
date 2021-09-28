2f69fa7e1009a461c15cecc7dbc538316a556309
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2021-09-27 21:37:11 -0400

    fix: .env file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(errorMiddleware)
 app.use(notFoundMiddleware)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const startServer =  async () => {
     try{
